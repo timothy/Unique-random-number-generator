2e6f68f5b83daea3c7458a2c59ed1ed2bfa7eb64
timothy <tbradford16@my.whitworth.edu>
2015-03-28 19:13:00 -0700

    Added min and max functionality
    Added min and max functionality and comments.

--- UniqueRand/src/uniquerand/UniqueRand.java
+++ UniqueRand/src/uniquerand/UniqueRand.java
@@ -20,26 +20,33 @@ public class UniqueRand {
     public static void main(String[] args) {
         // TODO code application logic here
         ArrayList<Integer> rand = new <Integer>ArrayList();
-        rand = gen(10, 5);
+        rand = gen(50, 10);// max, min -- max always has to be greater than min other wise return 0
         for (int i = 0; i < rand.size(); i++) {
             System.out.print(rand.get(i));
             System.out.print(" ");
         }
     }
 
-    public static ArrayList<Integer> gen(int size, int max) {
+    /**
+     *
+     * @param max the max random number that will occur
+     * @param min the min random number that will occur
+     * @return an ArrayList of unique numbers that are scrambled in a random
+     * sequence between max and min
+     */
+    public static ArrayList<Integer> gen(int max, int min) {
         ArrayList<Integer> uniquRan = new <Integer>ArrayList();
         int random = 0;
         int temp = 0;
         Random rand = new Random();
 
-        for (int i = 0; i < size; i++) {
+        for (int i = min; i < max; i++) {
             uniquRan.add(i);
         }
 
-        for (int i = 0; i < size; i++) {
-            random = rand.nextInt(size);
-            
+        for (int i = 0; i < max - min; i++) {
+            random = rand.nextInt(max - min);
+
             temp = uniquRan.get(i);
             uniquRan.set(i, uniquRan.get(random));
             uniquRan.set(random, temp);
